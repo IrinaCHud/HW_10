b7996aa39e230981b18d1780cb4dc5f13e3c0a91
IrinaCHud <120559343+IrinaCHud@users.noreply.github.com>
2023-03-12 16:11:36 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,32 @@
+const dataInfo=
+`[
+    {
+      "API": "AdoptAPet",
+      "Description": "Resource to help get pets adopted",
+      "Auth": "apiKey",
+      "HTTPS": true,
+      "Cors": "yes",
+      "img":"https://moe-online.ru/media_new/1/7/4/9/8/2/3/nn533329/original_photo-thumb_1920.jpg",
+      "Link": "https://www.adoptapet.com/public/apis/pet_list.html",
+      "Category": "Animals"
+    },
+    {
+      "API": "Axolotl",
+      "Description": "Collection of axolotl pictures and facts",
+      "Auth": "",
+      "HTTPS": true,
+      "Cors": "no",
+      "img":"https://tvorcoff.by/images/products/0-1632130077.jpg",
+      "Link": "https://theaxolotlapi.netlify.app/",
+      "Category": "Animals"
+    },
+    {
+      "API": "Cat Facts",
+      "Description": "Daily cat facts",
+      "Auth": "",
+      "HTTPS": true,
+      "Cors": "no",
+      "img":"https://cdn.fishki.net/upload/post/201604/06/1910378/tn/1063808.jpg",
+      "Link": "https://alexwohlbruck.github.io/cat-facts/",
+      "Category": "Animals"
+    }]`
